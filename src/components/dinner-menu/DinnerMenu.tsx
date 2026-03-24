70a933c4774b22e23fe7fa457203b600a8de6482
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2026-03-24 15:40:01 +0100

    Dinner menu global list yay

--- src/components/dinner-menu/DinnerMenu.tsx
+++ src/components/dinner-menu/DinnerMenu.tsx
@@ -1,36 +1,41 @@
-import styles from './DinnerMenu.module.css'
-import { dinnerMeals } from './data'
+import styles from './DinnerMenu.module.css';
+import { dinnerMeals } from './data';
+
+const categoryStarts = dinnerMeals.map((_, i) =>
+	dinnerMeals.slice(0, i).reduce((sum, c) => sum + c.meals.length, 1),
+);
 
 export const DinnerMenu = () => {
-  return(
-    <div className={styles.dinnerMenuContainer}>
-      <h2>Kvällsmeny</h2>
-      <div className={styles.gridContainer}>
-        {dinnerMeals.map((category) => (
-          <div key={category.mealCategory} className={styles.gridItem}>
-            <div className={styles.mealCategory}>{category.mealCategory}</div>
-            {category.meals.map((meal) => (
-              <div>
-                <div key={meal.text} className={styles.menuItem}>
-                  {meal.text}
-                  <span>{meal.price ? `${meal.price}kr` : ''}</span>
-                </div>
-                {meal.variants && (
-                  meal.variants.map((variant) => (
-                    <ul>
-                      <li>{variant}</li>
-                    </ul>
-                  ))
-                )}
-
-                
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-      
-    </div>
-   
-  )
-}
+	return (
+		<div className={styles.dinnerMenuContainer}>
+			<h2>Kvällsmeny</h2>
+			<div className={styles.gridContainer}>
+				{dinnerMeals.map((category, categoryIndex) => {
+					const categoryStart = categoryStarts[categoryIndex];
+					return (
+						<div key={category.mealCategory} className={styles.gridItem}>
+							<div className={styles.mealCategory}>{category.mealCategory}</div>
+							<ol start={categoryStart}>
+								{category.meals.map((meal) => (
+									<li key={meal.text}>
+										<div className={styles.menuItem}>
+											<span>{meal.text}</span>
+											<span>{meal.price ? `${meal.price}kr` : ''}</span>
+										</div>
+										{meal.variants && (
+											<ul>
+												{meal.variants.map((variant) => (
+													<li key={variant}>{variant}</li>
+												))}
+											</ul>
+										)}
+									</li>
+								))}
+							</ol>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
--- src/components/dinner-menu/data.ts
+++ src/components/dinner-menu/data.ts
@@ -1,139 +1,155 @@
-import type { DinnerMenu } from "../../types/types";
+import type { DinnerMenu } from '../../types/types';
 /* import { GiChiliPepper } from "react-icons/gi"; Försök få att funka*/
 
 export const dinnerMeals: DinnerMenu[] = [
-  {
-    mealCategory: 'Barnmeny',
-    meals: [
-      { text: 'Friterad Kyckling', price: 70 },
-      { text: 'Äggnudlar med kyckling', price: 70 },
-      { text: 'Kycklingvingar med pommes', price: 70 },
-    ]
-  },
-  {
-    mealCategory: 'Kyckling',
-    meals: [
-      { text: 'Wokad kyckling med bambuskott', price: 105 },
-      { text: 'Wokad kyckling med grönsaker', price: 105 },
-      { text: 'Wokad kyckling med grönsaker i szechuansås🌶️🌶️', price: 105 },
-      { text: 'Kyckling pokébowl', price: 105 },
-    ]
-  },
-  {
-    mealCategory: 'Biff',
-    meals: [
-      { text: 'Wokad biff med bambuskott', price: 115 },
-      { text: 'Wokad biff med blandade grönsaker', price: 115 },
-      { text: 'Wokad biff med grönsaker i szechuansås🌶️🌶️', price: 115 },
-      { text: 'Biff pokébowl', price: 115 },
-    ]
-  },
-  {
-    mealCategory: 'Fläsk',
-    meals: [
-      { text: 'Wokad fläsk med grönsaker', price: 105 },
-      { text: 'Wokad fläsk med grönsaker i szechuansås🌶️🌶️', price: 105 },
-    ]
-  },
-  {
-    mealCategory: 'Anka',
-    meals: [
-      { text: 'Friterad anka med blandade grönsaker', price: 119 },
-      { text: 'Friterad anka med blandade grönsaker i szechuansås🌶️🌶️', price: 119 },
-      { text: 'Friterad anka med lök, ananas i sötsursås', price: 119 },
-    ]
-  },
-  {
-    mealCategory: 'Räkor',
-    meals: [
-      { text: 'Wokade räkor med blandade grönsaker', price: 115 },
-      { text: 'Wokade räkor med blandade grönsaker i szechuansås🌶️🌶️', price: 115 },
-      { text: 'Räkor pokébowl', price: 115 },
-    ]
-  },
-  {
-    mealCategory: 'Nudlar',
-    meals: [
-      { text: 'Wokade äggnudlar med kyckling & blandade grönsaker', price: 100 },
-      { text: 'Wokade äggnudlar med biff & blandade grönsaker', price: 110 },
-      { text: 'Wokade äggnudlar med räkor & blandade grönsaker', price: 110 },
-      { text: 'Wokade äggnudlar med kyckling, biff samt räkor & blandade grönsaker', price: 110 },
-    ]
-  },
-  {
-    mealCategory: 'Stekt ris',
-    meals: [
-      { text: 'Stekt ris med kyckling & grönsaker', price: 105 },
-      { text: 'Stekt ris med biff & grönsaker', price: 115 },
-      { text: 'Stekt ris med räkor & grönsaker', price: 115 },
-      { text: 'Stekt ris med kyckling, räkor samt biff & grönsaker', price: 115 },
-    ]
-  },
-  {
-    mealCategory: 'PadThai🌶️',
-    meals: [
-      { text: 'Risnudlar med kyckling & blandade grönsaker', price: 105 },
-      { text: 'Risnudlar med räkor & blandade grönsaker', price: 115 },
-      { text: 'Risnudlar med räkor, biff samt kyckling & blandade grönsaker', price: 115 },
-      { text: 'Risnudlar med biff & blandade grönsaker', price: 115 },
-    ]
-  },
-  {
-    mealCategory: 'Friterad',
-    meals: [
-      { text: 'Friterad kycklingvingar & pommes', price: 105 },
-      { text: 'Friterad kyckling med sötsursås/jordnötssås', price: 105 },
-      { text: 'Friterad räkor med sötsursås/jordnötssås', price: 115 },
-      { text: 'Kycklingspett i jordnötssås/sötsursås', price: 105 },
-    ]
-  },
-  {
-    mealCategory: 'Små Rätter',
-    meals: [
-      {
-        text: 'Fyra små rätter',
-        price: 125,
-        variants: [
-          'Wokad biff med bambuskott',
-          'Friterad räkor med sötsursås',
-          'Friterad kyckling med sötsursås',
-          'Kyckling i gulcurry',
-        ]
-      },
-      {
-        text: 'Tre små rätter',
-        price: 115,
-        variants: [
-          'Wokad biff med bambuskott',
-          'Friterad räkor & sötsursås',
-          'Kycklingspett i jordnötssås',
-        ]
-      },
-    ]
-  },
-  {
-    mealCategory: 'Vegetariskt',
-    meals: [
-      { text: 'Wokad tofu med blandade grönsaker i szechuansås🌶️🌶️', price: 99 },
-      { text: 'Wokad tofu med blandade grönsaker', price: 99 },
-      { text: 'Vegetariska vårrullar, ris & sås samt sallader', price: 99 },
-      { text: 'Wokade äggnudlar med blandade grönsaker', price: 90 },
-    ]
-  },
-  {
-    mealCategory: 'Curry',
-    meals: [
-      {
-        text: 'Rödcurry med blandade grönsaker (Kyckling / räkor / tofu eller anka)🌶️',
-      
-      },
-      {
-        text: 'Gulcurry med blandade grönsaker (Kyckling / räkor eller tofu)🌶️',
-        
-      },
-      {
-        text: 'Panangcurry med lök, paprika & ananas (Kyckling / räkor / anka eller tofu) PRIIIIIIS',
-      },
-    ]
-  },
-]
+	{
+		mealCategory: 'Barnmeny',
+		meals: [
+			{ text: 'Friterad Kyckling', price: 70 },
+			{ text: 'Äggnudlar med kyckling', price: 70 },
+			{ text: 'Kycklingvingar med pommes', price: 70 },
+		],
+	},
+	{
+		mealCategory: 'Kyckling',
+		meals: [
+			{ text: 'Wokad kyckling med bambuskott', price: 105 },
+			{ text: 'Wokad kyckling med grönsaker', price: 105 },
+			{ text: 'Wokad kyckling med grönsaker i szechuansås🌶️🌶️', price: 105 },
+			{ text: 'Kyckling pokébowl', price: 105 },
+		],
+	},
+	{
+		mealCategory: 'Biff',
+		meals: [
+			{ text: 'Wokad biff med bambuskott', price: 115 },
+			{ text: 'Wokad biff med blandade grönsaker', price: 115 },
+			{ text: 'Wokad biff med grönsaker i szechuansås🌶️🌶️', price: 115 },
+			{ text: 'Biff pokébowl', price: 115 },
+		],
+	},
+	{
+		mealCategory: 'Fläsk',
+		meals: [
+			{ text: 'Wokad fläsk med grönsaker', price: 105 },
+			{ text: 'Wokad fläsk med grönsaker i szechuansås🌶️🌶️', price: 105 },
+		],
+	},
+	{
+		mealCategory: 'Anka',
+		meals: [
+			{ text: 'Friterad anka med blandade grönsaker', price: 119 },
+			{
+				text: 'Friterad anka med blandade grönsaker i szechuansås🌶️🌶️',
+				price: 119,
+			},
+			{ text: 'Friterad anka med lök, ananas i sötsursås', price: 119 },
+		],
+	},
+	{
+		mealCategory: 'Räkor',
+		meals: [
+			{ text: 'Wokade räkor med blandade grönsaker', price: 115 },
+			{
+				text: 'Wokade räkor med blandade grönsaker i szechuansås🌶️🌶️',
+				price: 115,
+			},
+			{ text: 'Räkor pokébowl', price: 115 },
+		],
+	},
+	{
+		mealCategory: 'Nudlar',
+		meals: [
+			{
+				text: 'Wokade äggnudlar med kyckling & blandade grönsaker',
+				price: 100,
+			},
+			{ text: 'Wokade äggnudlar med biff & blandade grönsaker', price: 110 },
+			{ text: 'Wokade äggnudlar med räkor & blandade grönsaker', price: 110 },
+			{
+				text: 'Wokade äggnudlar med kyckling, biff samt räkor & blandade grönsaker',
+				price: 110,
+			},
+		],
+	},
+	{
+		mealCategory: 'Stekt ris',
+		meals: [
+			{ text: 'Stekt ris med kyckling & grönsaker', price: 105 },
+			{ text: 'Stekt ris med biff & grönsaker', price: 115 },
+			{ text: 'Stekt ris med räkor & grönsaker', price: 115 },
+			{
+				text: 'Stekt ris med kyckling, räkor samt biff & grönsaker',
+				price: 115,
+			},
+		],
+	},
+	{
+		mealCategory: 'PadThai🌶️',
+		meals: [
+			{ text: 'Risnudlar med kyckling & blandade grönsaker', price: 105 },
+			{ text: 'Risnudlar med räkor & blandade grönsaker', price: 115 },
+			{
+				text: 'Risnudlar med räkor, biff samt kyckling & blandade grönsaker',
+				price: 115,
+			},
+			{ text: 'Risnudlar med biff & blandade grönsaker', price: 115 },
+		],
+	},
+	{
+		mealCategory: 'Friterad',
+		meals: [
+			{ text: 'Friterad kycklingvingar & pommes', price: 105 },
+			{ text: 'Friterad kyckling med sötsursås/jordnötssås', price: 105 },
+			{ text: 'Friterad räkor med sötsursås/jordnötssås', price: 115 },
+			{ text: 'Kycklingspett i jordnötssås/sötsursås', price: 105 },
+		],
+	},
+	{
+		mealCategory: 'Små Rätter',
+		meals: [
+			{
+				text: 'Fyra små rätter',
+				price: 125,
+				variants: [
+					'Wokad biff med bambuskott',
+					'Friterad räkor med sötsursås',
+					'Friterad kyckling med sötsursås',
+					'Kyckling i gulcurry',
+				],
+			},
+			{
+				text: 'Tre små rätter',
+				price: 115,
+				variants: [
+					'Wokad biff med bambuskott',
+					'Friterad räkor & sötsursås',
+					'Kycklingspett i jordnötssås',
+				],
+			},
+		],
+	},
+	{
+		mealCategory: 'Vegetariskt',
+		meals: [
+			{ text: 'Wokad tofu med blandade grönsaker i szechuansås🌶️🌶️', price: 99 },
+			{ text: 'Wokad tofu med blandade grönsaker', price: 99 },
+			{ text: 'Vegetariska vårrullar, ris & sås samt sallader', price: 99 },
+			{ text: 'Wokade äggnudlar med blandade grönsaker', price: 90 },
+		],
+	},
+	{
+		mealCategory: 'Curry',
+		meals: [
+			{
+				text: 'Rödcurry med blandade grönsaker (Kyckling / räkor / tofu eller anka)🌶️',
+			},
+			{
+				text: 'Gulcurry med blandade grönsaker (Kyckling / räkor eller tofu)🌶️',
+			},
+			{
+				text: 'Panangcurry med lök, paprika & ananas (Kyckling / räkor / anka eller tofu) PRIIIIIIS',
+			},
+		],
+	},
+];
