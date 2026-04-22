b59c9273699916fb83b3fdfc607e3907acac15c4
Filip Carlsson <bengtfille2@gmail.com>
2026-04-22 20:15:08 +0200

    fixes

--- src/components/lunch-menu/MenuEntry.tsx
+++ src/components/lunch-menu/MenuEntry.tsx
@@ -1,27 +1,27 @@
-import type { Meal } from '../../types/types';
-import styles from './MenuEntry.module.css';
+import type { Meal } from "../../types/types";
+import styles from "./MenuEntry.module.css";
 
 interface MenuEntryProps {
-	title: string;
-	meals: Meal[];
+  title: string;
+  meals: Meal[];
 }
 
 export function MenuEntry({ title, meals }: MenuEntryProps) {
-	return (
-		<article className={styles.menuEntry}>
-			<div className={styles.title}>{title}</div>
-			<ol>
-				{meals.map((meal) => (
-					<>
-						<li>{meal.text}</li>
-						{meal.variants?.map((variant) => (
-							<ul key={meal.text}>
-								<li>{variant}</li>
-							</ul>
-						))}
-					</>
-				))}
-			</ol>
-		</article>
-	);
+  return (
+    <article className={styles.menuEntry}>
+      <div className={styles.title}>{title}</div>
+      <ol>
+        {meals.map((meal) => (
+          <>
+            <li>{meal.text}</li>
+            {meal.variants?.map((variant) => (
+              <ul key={meal.text}>
+                <li>{variant}</li>
+              </ul>
+            ))}
+          </>
+        ))}
+      </ol>
+    </article>
+  );
 }
