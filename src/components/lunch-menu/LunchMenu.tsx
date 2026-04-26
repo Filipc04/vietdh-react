68a4f82a0cbd7f0e9c0aa46cc3effaec200f4593
Filip Carlsson <157843425+Filipc04@users.noreply.github.com>
2026-04-26 23:56:56 +0200

    Refactor LunchMenu to use dailyMeals array

--- src/components/lunch-menu/LunchMenu.tsx
+++ src/components/lunch-menu/LunchMenu.tsx
@@ -1,16 +1,8 @@
-import dayjs from 'dayjs';
-import isoWeek from 'dayjs/plugin/isoWeek';
-import { dailyMealsEven, dailyMealsOdd } from './data';
+import { dailyMeals } from './data';
 import styles from './LunchMenu.module.css';
 import { MenuEntry } from './MenuEntry';
 
 export function LunchMenu() {
-	dayjs.extend(isoWeek);
-	const currentWeek = dayjs().isoWeek();
-
-	const evenWeek = currentWeek % 2 === 0;
-	const meals = evenWeek ? dailyMealsEven : dailyMealsOdd;
-
 	return (
 		<div className={styles.lunchMenu}>
 			<h2>Lunchmeny</h2>
@@ -20,7 +12,7 @@ export function LunchMenu() {
 					<br />
 					Standard lunchpris: Avhämtning - 90kr, Servering - 95kr
 				</h4>
-				{meals.map((dayAndMeal) => (
+				{dailyMeals.map((dayAndMeal) => (
 					<MenuEntry
 						key={dayAndMeal.day}
 						title={dayAndMeal.day}
