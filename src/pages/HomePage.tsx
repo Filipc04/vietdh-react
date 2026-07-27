5a87e6e9732d2482bf67c58f39e7efa0a4824faf
Filip C <bengtfille2@gmail.com>
2026-07-27 19:59:20 +0200

    temp meddelande

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,24 +1,27 @@
 import {
-	DinnerMenu,
-	Footer,
-	Header,
-	LunchMenu,
-	RestaurantInfo,
-	Reviews,
-} from '../components';
-import styles from './HomePage.module.css';
+  DinnerMenu,
+  Footer,
+  Header,
+  LunchMenu,
+  RestaurantInfo,
+  Reviews,
+} from "../components";
+import styles from "./HomePage.module.css";
 
 export const HomePage = () => {
-	return (
-		<>
-			<Header />
-			<div className={styles.lunchAndInfo}>
-				<LunchMenu />
-				<RestaurantInfo />
-			</div>
-			<DinnerMenu />
-			<Reviews />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <h1 className={styles.tempHeader}>
+        Stängt den 30/7 (Torsdag)! Öppet igen fredag.
+      </h1>
+      <div className={styles.lunchAndInfo}>
+        <LunchMenu />
+        <RestaurantInfo />
+      </div>
+      <DinnerMenu />
+      <Reviews />
+      <Footer />
+    </>
+  );
 };
