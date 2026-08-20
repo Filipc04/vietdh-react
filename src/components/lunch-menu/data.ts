a64bb02f2d58ef636977cab753f519f1d2b21afb
Filip Carlsson <157843425+Filipc04@users.noreply.github.com>
2026-08-20 23:26:25 +0200

    Update data.ts

--- src/components/lunch-menu/data.ts
+++ src/components/lunch-menu/data.ts
@@ -61,7 +61,7 @@ export const dailyMeals: DailyMenu[] = [
     day: "Fredag",
     meals: [
       {
-        text: "Marry me chicken - kycklingbröstfile i krämig sås med soltorkade tomater, serveras med ris.",
+        text: "Marry me chicken - kycklingbröstfile i krämig sås med soltorkade tomater, serveras med ris",
       },
       {
         text: "Biff pokébowl",
