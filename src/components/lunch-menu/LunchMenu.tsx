b5b2dde18bb2b42863d947fbe57aba09b58a6016
Thao <thaotranphuong900@gmail.com>
2026-08-27 12:32:18 +0200

    Update lunch menu prices For students
    Uppdaterat lunch meny pris för studenter

--- src/components/lunch-menu/LunchMenu.tsx
+++ src/components/lunch-menu/LunchMenu.tsx
@@ -8,7 +8,7 @@ export function LunchMenu() {
 			<h2>Lunchmeny</h2>
 			<div className={styles.days}>
 				<h4>
-					Student - 80kr, med läsk - 85kr
+					Student - 85kr, med läsk - 90kr
 					<br />
 					Standard lunchpris: Avhämtning - 90kr, Servering - 95kr
 				</h4>
