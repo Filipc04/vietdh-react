faf6a71c67c620ea3c8d3c3896e1e48bfc79693f
Thao <thaotranphuong900@gmail.com>
2026-09-15 11:10:31 +0200

    Update LunchMenu.tsx

--- src/components/lunch-menu/LunchMenu.tsx
+++ src/components/lunch-menu/LunchMenu.tsx
@@ -8,7 +8,7 @@ export function LunchMenu() {
 			<h2>Lunchmeny</h2>
 			<div className={styles.days}>
 				<h4>
-					Idag stänger vi klockan 14.00. Välkommen åter!
+			
 					Student - 85kr, med läsk - 90kr
 					<br />
 					Standard lunchpris: Avhämtning - 90kr, Servering - 95kr
